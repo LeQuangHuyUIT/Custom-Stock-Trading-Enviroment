efc04cd7eaacda97016a49962f61442e4f5d8348
LeQuangHuyUIT <71214509+LeQuangHuyUIT@users.noreply.github.com>
2021-12-19 23:13:05 +0700

    Update StockTradingEnv.py

--- env/StockTradingEnv.py
+++ env/StockTradingEnv.py
@@ -26,14 +26,12 @@ class StockTradingEnv(gym.Env):
         self.reward_range = (0, MAX_ACCOUNT_BALANCE)
 
         # Actions of the format Buy x%, Sell x%, Hold, etc.
-        # self.action_space = spaces.Box(
-            # low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16)
-
-        self.action_space = spaces.Discrete(n= 3)
+        self.action_space = spaces.Box(
+            low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16)
 
         # Prices contains the OHCL values for the last five prices
         self.observation_space = spaces.Box(
-            low=-np.inf, high=np.inf, shape=(5, window_size+2), dtype=np.float32)
+            low=0, high=1, shape=(5, window_size+2), dtype=np.float16)
 
     def _next_observation(self):
         # Get the stock data points for the last 5 days and scale to between 0-1
@@ -61,23 +59,22 @@ class StockTradingEnv(gym.Env):
             self.df.loc[self.current_step, "Open"], self.df.loc[self.current_step, "Close"])
 
         action_type = action
-        amount = 1
 
         if action_type < 1:
             # Buy amount % of balance in shares
             total_possible = int(self.balance / current_price)
-            shares_bought = int(total_possible * amount)
+            # shares_bought = int(total_possible * amount)
             prev_cost = self.cost_basis * self.shares_held
-            additional_cost = shares_bought * current_price
+            additional_cost = total_possible * current_price
 
             self.balance -= additional_cost
             self.cost_basis = (
-                prev_cost + additional_cost) / (self.shares_held + shares_bought)
-            self.shares_held += shares_bought
+                prev_cost + additional_cost) / (self.shares_held + total_possible)
+            self.shares_held += total_possible
 
         elif action_type < 2:
             # Sell amount % of shares held
-            shares_sold = int(self.shares_held * amount)
+            shares_sold = self.shares_held
             self.balance += shares_sold * current_price
             self.shares_held -= shares_sold
             self.total_shares_sold += shares_sold
@@ -85,18 +82,15 @@ class StockTradingEnv(gym.Env):
 
         self.net_worth = self.balance + self.shares_held * current_price
 
-        reward = (-self.max_net_worth + self.net_worth) / self.max_net_worth
         if self.net_worth > self.max_net_worth:
             self.max_net_worth = self.net_worth
 
         if self.shares_held == 0:
             self.cost_basis = 0
-           
-        return reward
 
     def step(self, action):
         # Execute one time step within the environment
-        reward = self._take_action(action)
+        self._take_action(action)
 
         self.current_step += 1
         # print(f"current_step: {self.current_step}, len(df): {len(self.df)}")
@@ -109,10 +103,8 @@ class StockTradingEnv(gym.Env):
 
         delay_modifier = (self.current_step / MAX_STEPS)
 
-#         reward = (self.net_worth - INITIAL_ACCOUNT_BALANCE) 
+        reward = (self.net_worth - INITIAL_ACCOUNT_BALANCE) * (1 - (0.08/365) * (self.current_step%366))
         
-#         if reward != 0:
-#             reward /= abs(reward)
 
         obs = self._next_observation()
 
